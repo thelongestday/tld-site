635c671afb73f81db24f9c22c9a340ea279314ef
lemon <lemon@far.avalon>
2009-03-27 08:36:23 +0000

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :session_key => "_CHANGEME_session",
-  :secret      => "CHANGESESSION"
+  :session_key => "_site_session",
+  :secret      => "d3c3fda3e7054edb4e2a50941e2dd4e3"
 }
 
 # Use the database for sessions instead of the cookie-based default,
