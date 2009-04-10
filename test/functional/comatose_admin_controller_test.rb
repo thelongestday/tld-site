83c724d90309c006908f37caf6b28da420507a69
thelongestday <funker@thelongestday.net>
2009-04-10 14:13:20 +0100

    Test the comatose_admin is restricted to admin users. #2

--- test/functional/comatose_admin_controller_test.rb
+++ test/functional/comatose_admin_controller_test.rb
@@ -1,11 +1,30 @@
 require 'test_helper'
 
 class ComatoseAdminControllerTest < ActionController::TestCase
-  context "on GET to :comatose_admin calls :admin_required" do
+  include PunterTestHelper
+  fixtures :comatose_pages
+
+  context "on GET to :comatose_admin calls :admin_required when not logged in" do
     setup do
-      get :comatose_admin 
+      get :index
     end
-    ComatoseAdminController.expects(:admin_required)
+    should_redirect_to("Login page") { login_path }
+  end
+
+  context "on GET to :comatose_admin calls :admin_required when logged in as a user" do
+    setup do
+      login_as_user
+      get :index
+    end
+    should_redirect_to("Login page") { login_path }
+  end
+
+  context "on GET to :comatose_admin calls :admin_required when logged in as an admin" do
+    setup do
+      login_as_admin
+      get :index
+    end
+    should_respond_with :success
   end
 end
 
