0ee3c7feedb41446d10790e483d6cc086b43f070
thelongestday <funker@thelongestday.net>
2009-04-24 11:02:45 +0100

    Don't cache comatose pages.

--- config/initializers/comatose.rb
+++ config/initializers/comatose.rb
@@ -5,5 +5,6 @@ Comatose.configure do |config|
   config.admin_includes << :punter_system
   config.admin_authorization = :admin_required
   config.includes << :punter_system
+  config.disable_caching = true
   config.authorization = :page_filter
 end
